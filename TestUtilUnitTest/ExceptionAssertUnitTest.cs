c99662f056766a389d0f0a98e13340fdba78f819
Steve <mister.broshar@yahoo.com>
2016-10-14 21:51:47 -0500

    Initial add.

---
+++ TestUtilUnitTest/ExceptionAssertUnitTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUtils;
+
+namespace TestUtilsUnitTest
+{
+    [TestClass]
+    public class ExceptionAssertUnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void NonThrowingBlockIsTestFailure()
+        {
+            ExceptionAssert.Propagates<ExceptionClass>(() => { });
+        }
+
+        [TestMethod]
+        public void TestFailureMessageContainsFullNameOfExceptionClass()
+        {
+            try
+            {
+                ExceptionAssert.Propagates<ExceptionClass>(() => { });
+            }
+            catch (AssertFailedException e)
+            {
+                StringAssert.Contains(e.Message, typeof(ExceptionClass).FullName);
+            }
+        }
+
+        [TestMethod]
+        public void TestFailureMessageEndsWithMessageParameter()
+        {
+            try
+            {
+                ExceptionAssert.Propagates<ExceptionClass>(() => { }, "special msg");
+            }
+            catch (AssertFailedException e)
+            {
+                StringAssert.EndsWith(e.Message, "special msg");
+            }
+        }
+
+        [TestMethod]
+        public void TestFailureMessageEndsWithMessageParameterWhenIncludeValidation()
+        {
+            try
+            {
+                ExceptionAssert.Propagates<ExceptionClass>(() => { }, e => { }, "special msg");
+            }
+            catch (AssertFailedException e)
+            {
+                StringAssert.EndsWith(e.Message, "special msg");
+            }
+        }
+
+        [TestMethod]
+        public void BlockThatThrowsExpectedTypeIsNotTestFailure()
+        {
+            ExceptionAssert.Propagates<ExceptionClass>(() => { throw new ExceptionClass(); });
+        }
+
+        [TestMethod]
+        public void BlockThatThrowsExpectedTypeIsNotTestFailureForValidationMethod()
+        {
+            ExceptionAssert.Propagates<ExceptionClass>(() => { throw new ExceptionClass(); }, e => { });
+        }
+
+        [TestMethod]
+        public void BlockThatThrowsSubclassOfExpectedTypeIsNotTestFailure()
+        {
+            ExceptionAssert.Propagates<ExceptionClass>(() => { throw new ExceptionSubClass(); });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExceptionClass))]
+        public void BlockThatThrowsSuperclassOfExpectedTypePropagatesSuperclass()
+        {
+            ExceptionAssert.Propagates<ExceptionSubClass>(() => { throw new ExceptionClass(); });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception3))]
+        public void ValidationIsInvoked()
+        {
+            ExceptionAssert.Propagates<ExceptionClass>(
+                () => { throw new ExceptionClass(); },
+                e => { throw new Exception3(); });
+        }
+
+        private class ExceptionClass : Exception { }
+        private class ExceptionSubClass : ExceptionClass { }
+        private class Exception3 : ExceptionClass { }
+    }
+}
---
+++ TestUtilUnitTest/LoggableCollectionAssertUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUtils;
+
+namespace TestUtilsUnitTest
+{
+    [TestClass]
+    public class LoggableCollectionAssertUnitTest
+    {
+        [TestMethod]
+        public void ContainsPassesForContains()
+        {
+            LoggableCollectionAssert.Contains(new[] { 1 }, 1);
+        }
+
+        [TestMethod]
+        public void ContainsFailsForNotContains()
+        {
+            ExceptionAssert.Propagates<UnitTestAssertException>(() => LoggableCollectionAssert.Contains(new[] { 2 }, 1));
+        }
+
+        [TestMethod]
+        public void AreEqualPassesForEqual()
+        {
+            LoggableCollectionAssert.AreEqual(new[] { 1 }, new[] { 1 });
+        }
+
+        [TestMethod]
+        public void AreEqualFailsForUnequal()
+        {
+            ExceptionAssert.Propagates<UnitTestAssertException>(() => LoggableCollectionAssert.AreEqual(new[] { 0 }, new[] { 1 }));
+        }
+
+        [TestMethod]
+        public void AreEquivalentPassesForSameExceptForOrder()
+        {
+            LoggableCollectionAssert.AreEquivalent(new[] { 1, 2 }, new[] { 2, 1 });
+        }
+
+        [TestMethod]
+        public void AreEquivalentFailsForUnequal()
+        {
+            ExceptionAssert.Propagates<UnitTestAssertException>(() => LoggableCollectionAssert.AreEquivalent(new[] { 0 }, new[] { 1 }));
+        }
+    }
+}
